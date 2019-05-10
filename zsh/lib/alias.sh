0cac962233ec4acee95d9c06f7447a2d37657c64
mrdulin <novaline@qq.com>
2019-05-11 00:49:09 +0800

    add zsh-autosuggestions plugins as git submodule

--- zsh/lib/alias.sh
+++ zsh/lib/alias.sh
@@ -8,7 +8,7 @@
 # alias ohmyzsh="mate ~/.oh-my-zsh"
 
 # proxy list
-# alias proxy='export all_proxy=socks5://127.0.0.1:1086'
 
-alias proxy='export http_proxy=http://127.0.0.1:1087; https_proxy=https://127.0.0.1:1087'
-alias unproxy='unset all_proxy'
+alias sproxy-on='export http_proxy=socks5://127.0.0.1:1086; https_proxy=socks5://127.0.0.1:1086'
+alias hproxy-on='export http_proxy=http://127.0.0.1:1087; https_proxy=https://127.0.0.1:1087'
+alias proxy-off='unset http_proxy; unset https_proxy'
--- zsh/lib/gcloud.sh
+++ zsh/lib/gcloud.sh
@@ -1,5 +1,5 @@
 # The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/ldu020/google-cloud-sdk/path.zsh.inc' ]; then . '/Users/ldu020/google-cloud-sdk/path.zsh.inc'; fi
+if [ -f /Users/$USER/workspace/google-cloud-sdk/path.zsh.inc ]; then . /Users/$USER/workspace/google-cloud-sdk/path.zsh.inc; fi
 
 # The next line enables shell command completion for gcloud.
-if [ -f '/Users/ldu020/google-cloud-sdk/completion.zsh.inc' ]; then . '/Users/ldu020/google-cloud-sdk/completion.zsh.inc'; fi
+if [ -f /Users/$USER/workspace/google-cloud-sdk/completion.zsh.inc ]; then . /Users/$USER/workspace/google-cloud-sdk/completion.zsh.inc; fi
