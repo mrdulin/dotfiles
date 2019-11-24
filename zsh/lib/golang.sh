de224872f68a69ce0f112a36ba25d1541e147e2d
mrdulin <novaline.dulin@gmail.com>
2019-11-24 22:44:38 +0800

    update

--- zsh/lib/golang.sh
+++ zsh/lib/golang.sh
@@ -1,3 +1,3 @@
 # Golang
-export GOPATH=$HOME/workspace/go
-export PATH=$PATH:$GOPATH/bin
+export GOPATH=$HOME/workspace/go:$HOME/workspace/github.com/mrdulin/golang
+export PATH=$PATH:$HOME/workspace/go/bin:$HOME/workspace/github.com/mrdulin/golang/bin
